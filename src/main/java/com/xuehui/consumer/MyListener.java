de94cff0ef75e57aab82f3a6e140d0613aff220c
sojisubxh <xhjeonjh@163.com>
2019-04-26 09:09:01 +0800

    修改版本

--- src/main/java/com/xuehui/consumer/MyListener.java
+++ src/main/java/com/xuehui/consumer/MyListener.java
@@ -4,8 +4,10 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * <p>
@@ -35,12 +37,81 @@ public class MyListener {
     private static final String TPOIC = "xh_test";
 
     /**
-     * 监听topic,批量消费
+     * id是消费者监听容器,topicPartitions是配置topic和分区,partitions是指定只接受哪个分区的数据,可以配置多个
+     *
+     * @param records
      */
+    @KafkaListener(id = "id0", topicPartitions = {@TopicPartition(topic = TPOIC, partitions = {"0"})})
+    public void listenPartition0(List<ConsumerRecord<?, ?>> records) {
+        logger.info("Id0 Listener, Thread ID: " + Thread.currentThread().getId());
+        logger.info("Id0 records size " + records.size());
+
+        for (ConsumerRecord<?, ?> record : records) {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+            logger.info("Received: " + record);
+            if (kafkaMessage.isPresent()) {
+                Object message = record.value();
+                String topic = record.topic();
+                logger.info("p0 Received message={}", message);
+            }
+        }
+    }
+
+    @KafkaListener(id = "id1", topicPartitions = {@TopicPartition(topic = TPOIC, partitions = {"1"})})
+    public void listenPartition1(List<ConsumerRecord<?, ?>> records) {
+        logger.info("Id1 Listener, Thread ID: " + Thread.currentThread().getId());
+        logger.info("Id1 records size " + records.size());
+
+        for (ConsumerRecord<?, ?> record : records) {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+            logger.info("Received: " + record);
+            if (kafkaMessage.isPresent()) {
+                Object message = record.value();
+                String topic = record.topic();
+                logger.info("p1 Received message={}", message);
+            }
+        }
+    }
+
+    @KafkaListener(id = "id2", topicPartitions = {@TopicPartition(topic = TPOIC, partitions = {"2"})})
+    public void listenPartition2(List<ConsumerRecord<?, ?>> records) {
+        logger.info("Id2 Listener, Thread ID: " + Thread.currentThread().getId());
+        logger.info("Id2 records size " + records.size());
+
+        for (ConsumerRecord<?, ?> record : records) {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+            logger.info("Received: " + record);
+            if (kafkaMessage.isPresent()) {
+                Object message = record.value();
+                String topic = record.topic();
+                logger.info("p2 Received message={}", message);
+            }
+        }
+    }
+
+    @KafkaListener(id = "id3", topicPartitions = {@TopicPartition(topic = TPOIC, partitions = {"3"})})
+    public void listenPartition3(List<ConsumerRecord<?, ?>> records) {
+        logger.info("Id3 Listener, Thread ID: " + Thread.currentThread().getId());
+        logger.info("Id3 records size " + records.size());
+
+        for (ConsumerRecord<?, ?> record : records) {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+            logger.info("Received: " + record);
+            if (kafkaMessage.isPresent()) {
+                Object message = record.value();
+                String topic = record.topic();
+                logger.info("p3 Received message={}", message);
+            }
+        }
+    }
+
+    /**
+     * 监听topic,批量消费
+     *//*
     @KafkaListener(topics = TPOIC)
     public void listen(List<ConsumerRecord<String, String>> records) {
         batchConsumer(records);
-    }
+    }*/
 
     /**
      * 单条消费
